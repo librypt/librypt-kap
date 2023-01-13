3d61a5588aa99c95b4107879329be9ece0dbb829
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-12 17:28:15 -0800

    Defined KAP traits.

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use librypt_entropy::EntropySource;
+
+/// A Key Agreement Protocol (KAP).
+pub trait KeyAgreementProtocol {
+    /// Custom type for shared data to facilitate the key exchange.
+    type SharedKey;
+
+    /// Custom type for the resulting shared secret.
+    ///
+    /// WARNING: The computed secret should **not** be used directly as key material, but rather fed through a Key Derivation Function (KDF).
+    type SharedSecret: AsRef<[u8]>;
+
+    /// Randomly generate a ephemeral key for the key exchange.
+    fn new(es: &impl EntropySource) -> Self;
+
+    /// Compute the public/shared parts of the ephemeral key.
+    fn shared_key(&self) -> Self::SharedKey;
+
+    /// Compute the shared secret given an external public/shared key.
+    fn compute_shared_secret(&self, shared_key: &Self::SharedKey) -> Self::SharedSecret;
+}
